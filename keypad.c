bd78bca3a4a6216afd707fb8c0a177b1758e0642
uriel garcia <urielgarcia@email.arizona.edu>
2014-10-06 16:21:21 -0700

    keypad , first try, of many

--- keypad.c
+++ keypad.c
@@ -10,12 +10,45 @@ void KeypadInitialize() {
 	// TODO: Configure IOs and Change Notificaiton interrupt for keypad scanning. This 
 	// configuration should ensure that if any key is pressed, a change notification interrupt 
 	// will be generated.
+
+    TRISBbits.TRISB11 = 0 //  row 1, output
+    TRISBbits.TRISB10 = 0; // row 2 , output
+    TRISBbits.TRISB9 = 0; // row 3 , output
+    TRISBbits.TRISB8 = 0; // row 4 output
+
+    ODCBbits.ODB11 = 1; // open drain configuration for rows
+    ODCBbits.ODB10 = 1;
+    ODCBbits.ODB9 = 1;
+    ODCBbits.ODB8 = 1;
+
+    TRISAbits.TRISA0 = 1; // column 1, input
+    TRISAbits.TRISA1 = 1; // column 2 , input
+    TRISBbits.TRISB2 = 1; // column 3 , input
+
+    CNPU1bits.CN2PUE = 1; // pull up resistor for columns
+    CNPU1bits.CN3PUE = 1;
+    CNPU1bits.CN6PUE = 1;
+
+    AD1PCFGbits.PCFG0 = 1; // digital configuration for the inputs, columns
+    AD1PCFGbits.PCFG1 = 1;
+    AD1PCFGbits.PCFG4 = 1;
+
+    IFS1bits.CNIF = 0; // enable change notification interrupt
+    IEC1bits.CNIE = 1;
+
+    CNEN1bits.CN2IE = 1; // change notificatio enable for the columns, inputs
+    CNEN1bits.CN3IE = 1;
+    CNEN1bits.CN6IE = 1;
+
+
+
 }
 
 // ******************************************************************************************* //
 
 char KeypadScan() {
 	char key = -1;
+        int scan = 0;
 	
 	
 	// TODO: Implement the keypad scanning procedure to detect if exactly one button of the 
@@ -34,5 +67,7 @@ char KeypadScan() {
 	//           is processed. This is to prevent invalid keypress from being processed if the 
 	//           users presses multiple keys simultaneously.
 	//
+
+
 	return key;
 }

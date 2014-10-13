a6c20ebf4b86b6ab48f07545de7e80352bd7a802
jesuschavolla <jesuschavolla@email.arizona.edu>
2014-10-12 22:43:29 -0700

    Still working on the usermode on comparing if it is a default password.

--- lab2.c
+++ lab2.c
@@ -6,6 +6,7 @@
 #include "p24fj64ga002.h"
 #include <stdio.h>
 #include "lcd.h"
+//#include "keypad.h"
 
 // ******************************************************************************************* //
 // Configuration bits for CONFIG1 settings. 
@@ -32,13 +33,25 @@ _CONFIG2( IESO_OFF & SOSCSEL_SOSC & WUTSEL_LEG & FNOSC_PRIPLL & FCKSM_CSDCMD & O
 // and the KeypadScan() function needs to be called.
 
 volatile char scanKeypad;
-
+volatile unsigned char cnt;
 // ******************************************************************************************* //
 
 int main(void)
 {
 	char key;
-        int i=0;
+        int count=0;//keeps track of how many times each key was pressed.
+        int state;//keeps track of current state
+        int doublecheck=0;
+//        char password[] ={'1','2','3','4'};
+//        char temporary[4];
+
+        PR1 = 57599;//1 second delay
+	TMR1 = 0;//resets timer 1
+	IFS0bits.T1IF = 0;
+	IEC0bits.T1IE = 1;
+	T1CONbits.TCKPS = 3;
+	T1CONbits.TON = 1;
+
 
 	// TODO: Initialize and configure IOs, LCD (using your code from Lab 1), 
 	// UART (if desired for debugging), and any other configurations that are needed.
@@ -47,28 +60,118 @@ int main(void)
 	KeypadInitialize();
 	
 	// TODO: Initialize scanKeypad variable.
-       LCDMoveCursor(0,0);
-       LCDPrintString("Enter");
-       LCDMoveCursor(1,0);
-       LCDPrintString("Password");
+     
+       state=0;
 	while(1)
 	{
 		// TODO: Once you create the correct keypad driver (Part 1 of the lab assignment), write
 		// the C program that use both the keypad and LCD drivers to implement the 4-digit password system.
-	
-              
-               
-                   
-		if( scanKeypad == 1 ) {
-                    for(i=0;i<=14400;i++);
-			key = KeypadScan();
-			if( key != -1 ) {
-				LCDMoveCursor(0,0);		
+
+            if(state==0){//enter mode
+                LCDMoveCursor(0,0);
+                LCDPrintString("Enter");
+                count=0;
+                doublecheck=0;
+                state=1;
+            }
+            if(scanKeypad==1)
+            {
+                key = KeypadScan();
+                
+                switch(state){
+
+                  case 1://User Mode
+			if( key != -1 && key!='*'&& key !='#' && doublecheck==0) {
+                            if(count==0){
+				LCDMoveCursor(1,count);
 				LCDPrintChar(key);
-			}
+                               // temporary[count]='key';
+                                count++;
+                            }
+                            else if (count==1){
+                                LCDMoveCursor(1,count);
+				LCDPrintChar(key);
+                               // temporary[count]='key';
+                                count++;
+                            }
+                            else if (count==2){
+                                LCDMoveCursor(1,count);
+				LCDPrintChar(key);
+                              //  temporary[count]='key';
+                                count++;
+                            }
+                            else if (count==3){
+                                LCDMoveCursor(1,count);
+				LCDPrintChar(key);
+                              //  temporary[count]='key';
+                                count++;
+                            }
+//                            else if(count==4)//after password was made, determine if its good
+//                            {
+//                                if(password==temporary){
+//                                    LCDMoveCursor(0,0);
+//                                    LCDPrintString("Good");
+//                                    cnt=0;
+//                                    while(cnt<3);//2 second delay;
+//                                    LCDClear();
+//
+//                                }
+//                            }
+
+                        }
+                        else if( key != -1 && (key=='*'|| key =='#'))
+                        {
+                            
+                                if(count==0 && key=='*')
+                                {
+                                     LCDMoveCursor(1,count);
+                                     LCDPrintChar(key);
+                                     count++;
+                                     doublecheck==1;
+                                }
+                                else if(count==1 && key== '*')
+                                {
+                                     LCDMoveCursor(1,count);
+                                     LCDPrintChar(key);
+                                     count=0;
+                                     doublecheck=0;
+                                     state=2;
+                                     break;
+                                }
+                                else if(count<2&& key=='#')
+                                {
+                                    LCDClear();
+                                    LCDMoveCursor(0,0);
+                                    LCDPrintString("Bad");
+                                    cnt=0;
+                                    while(cnt<3);//2 second delay;
+                                    LCDClear();
+                                    state=0;
+                                    break;
+                                }
+                                else{
+                                    LCDMoveCursor(0,0);
+                                    LCDClear();
+                                    LCDMoveCursor(0,0);
+                                    LCDPrintString("Bad");
+                                    cnt=0;
+                                    while(cnt<3);//2 second delay;
+                                    LCDClear();
+                                    state=0;
+                                    break;
+                                }
+                        }
 			scanKeypad = 0;
-		}
-        
+                    break;
+
+                    case 2://Program mode
+                        LCDClear();
+                        LCDMoveCursor(0,0);
+                        LCDPrintString("Set Mode");
+                        break;
+                }
+            }
+      
 	}
 	return 0;
 }
@@ -98,9 +201,17 @@ void __attribute__((interrupt)) _CNInterrupt(void)
              scanKeypad=1;
         else
             scanKeypad=0;
+}
+void __attribute__((interrupt,auto_psv)) _T1Interrupt(void)
+//void _ISR _T1Interrupt(void)
 
-
+{
+	// Clear Timer 1 interrupt flag to allow another Timer 1 interrupt to occur.
+	IFS0bits.T1IF = 0;
+        
+        cnt = (cnt<9)?(cnt+1):0;//counts the seconds
 
 }
+
 
 // ******************************************************************************************* //
